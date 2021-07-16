3e1bc13111da50eca9e095bf8be8c7bd252c35a5
codymart1986 <codymart1986@hotmail.com>
2021-07-15 19:32:07 -0700

    Created fleet class and extended it into main

--- robots_vs_dinos/Fleet.py
+++ robots_vs_dinos/Fleet.py
@@ -1,3 +1,7 @@
 class Fleet:
     def __init__(self):
         #create fleet
+        self.robo_fleet = []
+
+    def add_robo(self,robo):
+        self.robo_fleet.insert(robo)
--- robots_vs_dinos/main.py
+++ robots_vs_dinos/main.py
@@ -1,2 +1,23 @@
+from robots import Robot
+from dino import Dino
+from Fleet import Fleet
+from herd import Herd
+
 import random
+
+#Robots Created
+robo1 = Robot("C-3PO")
+robo2 = Robot("Wall-E")
+robo3 = Robot("Cl4pTp")
+
+#Dino's Created
+dino1 = Dino("Rex")
+dino2 = Dino("Steg")
+dino3 = Dino("PaRappa the Raptor")
+
+#Adding Robots to a fleet
+fleet = Fleet()
+fleet.add_robo(robo1)
+fleet.add_robo(robo2)
+fleet.add_robo(robo3)
 
