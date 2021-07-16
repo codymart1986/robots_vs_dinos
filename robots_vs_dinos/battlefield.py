0abc639bf0f91793c69b7df9acb3a804affbd28a
codymart1986 <codymart1986@hotmail.com>
2021-07-15 20:08:23 -0700

    finished setting up classes, working on battlefield class currently. successfully launched attack from dino on robots. continuing work on finishing out a battle

--- robots_vs_dinos/battlefield.py
+++ robots_vs_dinos/battlefield.py
@@ -1,11 +1,42 @@
- class Battlefield:
-     def __init__(self) -> None:
-         #insert fleet and herd
-         #run_game
-         #display_welcome
-         #battle
-         #dino_turn
-         #robo_turn
-         #show_dino_opponent_options
-         #show_robo_opponent_options
-         #display winners
+class Battlefield:
+    def __init__(self, robos, dinos):
+        #insert fleet and herd
+        self.robos = robos
+        self.dinos = dinos
+        self.robos_turn = False
+
+        #display_welcome
+    def display_welcome():
+        print('Welcome to the Thunder Dome')
+
+    #battle
+    def battle(self):
+        #for robots in self.robos:
+        for dino in self.dinos:
+            if(dino.health > 0)
+
+    #Attack 
+    def attack(self, robo, dino):
+        if(self.robos_turn):
+            dino.health -= robo.weapon.attack_power
+            self.robos_turn = False
+        if(self.robos_turn == False):
+            robo.health -= dino.attack_power
+            self.robos_turn = True
+
+        print('Dino HP: ' + str(dino.health))
+        print('Robo HP: ' + str(robo.health))
+
+
+    #dino_turn
+    def robos_turn(self):
+        if(self.robos_turn == False):
+            return True
+    
+    #robo_turn
+    def robos_turn(self):
+        if(self.robos_turn):
+            return True
+
+
+    #display winners
